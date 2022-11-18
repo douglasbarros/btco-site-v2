360643347baf080eb7e18f57d15e46193829162b
douglasbarros <dbs.douglas@gmail.com>
2022-11-18 16:35:52 -0300

    Fixes.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,8 @@ import { FC } from "react";
 import Container from "./container";
 import Logo from "./logo";
 
+type ImageProps = { size?: number };
+
 const Footer: FC<Record<string, never>> = function () {
   const { t } = useTranslation("common");
   return (
@@ -16,7 +18,7 @@ const Footer: FC<Record<string, never>> = function () {
               <Link href="/">
                 <a
                   className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
-                  href="#"
+                  href="/#"
                 >
                   <span>
                     <Logo />
@@ -41,7 +43,7 @@ const Footer: FC<Record<string, never>> = function () {
                 rel="noreferrer"
               >
                 <span className="sr-only">Twitter</span>
-                <Twitter size={24} />
+                <Twitter />
               </a>
               <a
                 href="https://github.com/BitcoinNanoLabs/btco-wallet-flutter"
@@ -49,7 +51,7 @@ const Footer: FC<Record<string, never>> = function () {
                 rel="noreferrer"
               >
                 <span className="sr-only">GitHub</span>
-                <GitHub size={24} />
+                <GitHub />
               </a>
               <a
                 href="https://instagram.com/bitcoinnano/"
@@ -57,7 +59,7 @@ const Footer: FC<Record<string, never>> = function () {
                 rel="noreferrer"
               >
                 <span className="sr-only">Instagram</span>
-                <Instagram size={24} />
+                <Instagram />
               </a>
               <a
                 href="https://www.linkedin.com/company/bitcoin-nano/"
@@ -65,7 +67,7 @@ const Footer: FC<Record<string, never>> = function () {
                 rel="noreferrer"
               >
                 <span className="sr-only">Linkedin</span>
-                <Linkedin size={24} />
+                <Linkedin />
               </a>
             </div>
           </div>
@@ -79,7 +81,7 @@ const Footer: FC<Record<string, never>> = function () {
   );
 };
 
-const Twitter = ({ size = 24 }) => (
+const Twitter = ({ size = 24 }: ImageProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -91,7 +93,7 @@ const Twitter = ({ size = 24 }) => (
   </svg>
 );
 
-const GitHub = ({ size = 24 }) => (
+const GitHub = ({ size = 24 }: ImageProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -102,7 +104,7 @@ const GitHub = ({ size = 24 }) => (
     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
   </svg>
 );
-const Instagram = ({ size = 24 }) => (
+const Instagram = ({ size = 24 }: ImageProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
@@ -114,7 +116,7 @@ const Instagram = ({ size = 24 }) => (
   </svg>
 );
 
-const Linkedin = ({ size = 24 }) => (
+const Linkedin = ({ size = 24 }: ImageProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
